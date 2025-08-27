97749e40354338cfd72ced9b72839088fd27c692
Fábio Dantas <fabio.tot.brisa@gmail.com>
2025-08-26 22:55:28 -0300

    Adicionando a funcionalidade de voz inclusiva

--- app.js
+++ app.js
@@ -1,19 +1,85 @@
-let numeroSecreto = gerarNumeroaleatorio();
+//Variaveis globais: Numero a ser gerado automaticamente e chute passado pelo cliente, tentativas jogadas e lista de números que já foram sorteados
+let numeroSorteado = [];;
+let numeroSecreto = gerarNumeroAleatorio();
+let chute;
+let tentativas = 0;
 
+
+//Gerar numero aleatorio assim que o programa for iniciado
+function gerarNumeroAleatorio(){
+    let listaNumeroEscolhido = parseInt(Math.random() * 100 + 1);
+    let quantidadeDeElementosNaLista = listaNumeroEscolhido.length;
+
+    if (quantidadeDeElementosNaLista == 3){
+        listaNumeroEscolhido = [];
+    }
+
+    if (numeroSorteado.includes(listaNumeroEscolhido)) {
+        return gerarNumeroAleatorio();
+    }
+    else {
+        numeroSorteado.push(listaNumeroEscolhido);
+        console.log(numeroSorteado);
+        return listaNumeroEscolhido;
+    }
+}
+
+//Função que exibe textos na tela
 function exibirTextoTela(tag, texto) {
     let titulo = document.querySelector(tag);
-    titulo.innerHTML = texto; 
+    titulo.innerHTML = texto;
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.4});
 }
 
-exibirTextoTela('h1', 'DE 0 A 100');
-exibirTextoTela('p', 'Acerte o número e beba!');
+//Função para limpar campo de texto do chute
+function limparCampo(){
+    chute = document.querySelector('input');
+    chute.value = ' ';
+}
+
+//Reiniciar jogo (Função do botão novo jogo)
+function reiniciarJogo(){
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 0;
+    mensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', true);
+}
+
+//Mensagem inicial
+function mensagemInicial(){
+    exibirTextoTela('h1', 'DE 0 A 100');
+    exibirTextoTela('p', 'Acerte e beba!');
+}
+
+//Inicialização do programa
+mensagemInicial();
 
 function verificarChute(){
-    let chute = document.querySelector('input').value;
-    console.log(chute == numeroSecreto);
-    console.log(numeroSecreto);
+        tentativas++
+        pluralSingularTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        mensagem = `Número: ${numeroSecreto}. | Jogadas: ${tentativas} ${pluralSingularTentativa}.`
+        chute = parseInt(document.querySelector('input').value);
+        if (chute === numeroSecreto ){
+            exibirTextoTela('h1', 'Você bebeu!');
+            exibirTextoTela('p', mensagem);
+            document.getElementById('reiniciar').removeAttribute('disabled');
+        }
+        else if (chute > numeroSecreto && chute < 101){
+            exibirTextoTela('h1', 'Tente novamente!');
+            exibirTextoTela('p', `O número é MENOR que ${chute}`);
+            limparCampo();
+        }
+        else if (chute < numeroSecreto && chute > 0){
+            exibirTextoTela('h1', 'Tente novamente!');
+            exibirTextoTela('p', `O número é MAIOR que ${chute}`);
+            limparCampo();
+        }
+        else{
+            exibirTextoTela('h1', 'DIGITE UM NÚMERO VÁLIDO!!');
+            exibirTextoTela('h1', 'NÃO SEJA ESTUPIDO');
+            limparCampo();
+        }
 }
 
-function gerarNumeroaleatorio(){
-    return parseInt(Math.random() * 100 + 1);
-}
+ 
